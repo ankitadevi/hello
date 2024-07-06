3c06303b4d0a3e9e541e2492db2441f5e4a575de
Ankita Devi <ad2004ankita@gmail.com>
2024-07-06 10:11:55 +0530

    first commit

---
+++ main - Copy.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "ML Enthusiast","Programmer",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+
+ 
